082cff00602534e45ea9c996874b06a1b2418d53
lalyos <lalyos@yahoo.com>
2019-07-09 11:30:53 +0200

    fix nginx-ingress deploy
    0.25.0 movved mandatory.yaml into a different directory ....

--- workshop-functions.sh
+++ workshop-functions.sh
@@ -370,7 +370,7 @@ EOF
   else
     # https://kubernetes.github.io/ingress-nginx/deploy/#gce-gke
     echo "---> create: ns,cm,sa,crole,dep"
-    kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
     echo "---> creates single LB" 
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
   fi
